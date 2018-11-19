bfbf4c2f563bd034e67a5089687c305db993cbb2
Leonard <leonardjohnphillips@gmail.com>
2018-11-19 22:33:47 +1300

    Google Map added to contact.html

---
+++ src/js/google_map_script.js
@@ -0,0 +1,27 @@
+var CHBVetsMap;
+/* Initializes CHB Vet map */
+function initMap() {
+    var myLatLng = {lat: -39.994738, lng: 176.554233};
+    
+    /* Sets the location and aerial zoom level */
+    CHBVetsMap = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 18
+    });
+    
+    /* Set location of marker */
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: CHBVetsMap,
+        title: 'CHB Vets Ltd'
+    });
+    
+    /* Ties info box to marker and activates when marker is clicked */
+    var contentString = '<p><b>CHB Vets Ltd</b></p>' + '<p>7 Northumberland St<br>Waipukurau<br>4200<br>New Zealand</p>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    marker.addListener('click', function() {
+        infowindow.open(CHBVetsMap, marker);
+    });
+}
